c857657bde1924c6631485e3844423a8240503e0
Grellenort <k.krzyspiak@gmail.com>
2021-06-01 21:31:27 +0200

    Upgrade to Auto-Staking

--- src/connectors/ApiConnector.ts
+++ src/connectors/ApiConnector.ts
@@ -15,3 +15,43 @@ export async function getCirculation(): Promise<{
     return { success: false }
   }
 }
+
+export async function getEarned(
+  address: any
+): Promise<{
+  success: boolean
+  data?: { earned: number }
+}> {
+  try {
+    const response = await fetch(`https://private-staking-api.vaiot.ai/earned?` + address)
+    if (!response.ok) {
+      return { success: false }
+    } else {
+      const json = await response.json()
+      return { success: true, data: json }
+    }
+  } catch (error) {
+    console.log(error)
+    return { success: false }
+  }
+}
+
+export async function getTotal(
+  address: any
+): Promise<{
+  success: boolean
+  data?: { total: number }
+}> {
+  try {
+    const response = await fetch(`https://private-staking-api.vaiot.ai/total?` + address)
+    if (!response.ok) {
+      return { success: false }
+    } else {
+      const json = await response.json()
+      return { success: true, data: json }
+    }
+  } catch (error) {
+    console.log(error)
+    return { success: false }
+  }
+}
--- src/pages/PreStake/index.tsx
+++ src/pages/PreStake/index.tsx
@@ -1,36 +1,27 @@
 import { unwrappedToken } from '../../utils/wrappedCurrency'
 import { useCurrency } from '../../hooks/Tokens'
 import { currencyId } from '../../utils/currencyId'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { RowBetween } from '../../components/Row'
 import { TYPE } from '../../theme'
 import { AutoColumn } from '../../components/Column'
-import {
-  BottomSection,
-  DataRow,
-  PageWrapper,
-  PoolData,
-  PositionInfo,
-  StyledBottomCard,
-  StyledDataCard
-} from '../Earn/Manage'
+import { BottomSection, DataRow, PageWrapper, PositionInfo, StyledBottomCard, StyledDataCard } from '../Earn/Manage'
 import { usePreStakingInfo } from '../../state/prestake/hooks'
 import CurrencyLogo from '../../components/CurrencyLogo'
 import { useTokenBalance } from '../../state/wallet/hooks'
 import { useActiveWeb3React } from '../../hooks'
 import { useColor } from '../../hooks/useColor'
 import { JSBI, TokenAmount } from '@uniswap/sdk'
-import usePrevious from '../../hooks/usePrevious'
 import PreStakingModal from '../../components/PreStake/PreStakingModal'
 // import { useWalletModalToggle } from '../../state/application/hooks'
 import UnstakingModal from '../../components/PreStake/UnstakingModal'
 import { CardBGImage, CardNoise, CardSection } from 'components/earn/styled'
 import { ButtonPrimary } from '../../components/Button'
-import { CountUp } from 'use-count-up'
 import { VAI } from '../../constants'
 import { matchPath } from 'react-router'
 import { useLocation } from 'react-router-dom'
 import { useLockupInfo } from '../../state/lockup/hooks'
+import { getEarned, getTotal } from '../../connectors/ApiConnector'
 
 export function parseBigNumber(value: TokenAmount): string {
   return value.greaterThan(BigInt(100000))
@@ -66,8 +57,21 @@ export default function PreStake() {
 
   const backgroundColor = useColor(token)
 
-  const countUpAmount = stakingInfo?.earnedAmount?.toFixed(6) ?? '0'
-  const countUpAmountPrevious = usePrevious(countUpAmount) ?? '0'
+  const [earned, setEarned] = useState(0)
+  const [total, setTotal] = useState(0)
+  useEffect(() => {
+    const fetchData = async () => {
+      const currentEarned = await getEarned(account)
+      const currentTotal = await getTotal(account)
+      if (currentEarned.success && currentEarned.data?.earned) {
+        setEarned(currentEarned.data.earned)
+      }
+      if (currentTotal.success && currentTotal.data?.total) {
+        setTotal(currentTotal.data.total)
+      }
+    }
+    fetchData()
+  }, [account])
   return (
     <PageWrapper gap="lg" justify="center">
       <RowBetween style={{ gap: '24px' }}>
@@ -77,9 +81,7 @@ export default function PreStake() {
         <CurrencyLogo currency={currency0} size={'24'} />
       </RowBetween>
       <TYPE.white style={{ justifyContent: 'center', textAlign: 'justify' }}>
-        You can already stake all your purchased VAI Tokens. In addition to the pre-staking, you can also use this site
-        to unlock your VAI Tokens purchased in the Private Sale according to the unlock schedule presented below. To do
-        that, please tap on Unlock button. <br />
+        <br />
         <br />
         <TYPE.white style={{ justifyContent: 'center', textAlign: 'center', display: 'block' }}>
           <b>
@@ -87,37 +89,12 @@ export default function PreStake() {
           </b>
           <br />
           <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-            <div style={{ marginBottom: '10px', marginTop: '10px' }}> 15th April</div>
-            <div style={{ marginBottom: '10px' }}>15th May</div>
+            <br />
             <div style={{ marginBottom: '10px' }}>15th June</div>
             <div>15th July</div>
           </div>
         </TYPE.white>
       </TYPE.white>
-
-      <DataRow style={{ gap: '24px' }}>
-        <PoolData>
-          <AutoColumn gap="sm">
-            <TYPE.body style={{ margin: 0 }}>Total deposits</TYPE.body>
-            <RowBetween style={{ alignItems: 'baseline' }}>
-              <TYPE.body fontSize={24} fontWeight={500}>
-                {stakingInfo && stakingInfo.totalStakedAmount ? parseBigNumber(stakingInfo.totalStakedAmount) : '0'} /{' '}
-                {stakingInfo && stakingInfo.currentStakingLimit ? parseBigNumber(stakingInfo.currentStakingLimit) : '0'}
-              </TYPE.body>
-              <TYPE.white>VAI</TYPE.white>
-            </RowBetween>
-          </AutoColumn>
-        </PoolData>
-        <PoolData>
-          <AutoColumn gap="sm">
-            <TYPE.body style={{ margin: 0 }}>Pool Rate</TYPE.body>
-            <TYPE.body fontSize={24} fontWeight={500}>
-              {stakingInfo ? stakingInfo?.totalRewardRate.toString() : '0'}
-              {'% ARR + 0.2% daily'}
-            </TYPE.body>
-          </AutoColumn>
-        </PoolData>
-      </DataRow>
 
       {stakingInfo && (
         <>
@@ -149,7 +126,7 @@ export default function PreStake() {
                 </RowBetween>
                 <RowBetween style={{ alignItems: 'baseline' }}>
                   <TYPE.white fontSize={36} fontWeight={600}>
-                    {stakingInfo?.stakedAmount?.toSignificant(6) ?? '-'}
+                    {total ?? '-'}
                   </TYPE.white>
                   <TYPE.white>VAI</TYPE.white>
                 </RowBetween>
@@ -167,15 +144,7 @@ export default function PreStake() {
               </RowBetween>
               <RowBetween style={{ alignItems: 'baseline' }}>
                 <TYPE.largeHeader fontSize={36} fontWeight={600}>
-                  <CountUp
-                    key={countUpAmount}
-                    isCounting
-                    decimalPlaces={4}
-                    start={parseFloat(countUpAmountPrevious)}
-                    end={parseFloat(countUpAmount)}
-                    thousandsSeparator={','}
-                    duration={1}
-                  />
+                  {earned ?? 0}
                 </TYPE.largeHeader>
               </RowBetween>
             </AutoColumn>
@@ -183,25 +152,14 @@ export default function PreStake() {
         </BottomSection>
 
         <DataRow style={{ marginBottom: '1rem' }}>
-          {(stakingInfo?.stakedAmount?.greaterThan(JSBI.BigInt(0))) && stakingInfo?.isLockup && (
+          {stakingInfo?.stakedAmount?.greaterThan(JSBI.BigInt(0)) && stakingInfo?.isLockup && (
             <>
               <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={() => setShowUnstakingModal(true)}>
-                Withdraw
+                Upgrade to Auto-Staking
               </ButtonPrimary>
             </>
           )}
         </DataRow>
-
-        <TYPE.white style={{ justifyContent: 'center', textAlign: 'justify' }}>
-          <b>Important!</b> You can unlock your VAI only once every lockup period (30 days) on the days specified in the
-          token unlock schedule. If you try to tap on Unlock button earlier before the actual unlock dates, you won't
-          get your VAI, but you will still pay the transaction fee!
-        </TYPE.white>
-
-        <TYPE.white>
-          Detailed Pre-Staking rules available{' '}
-          <a href={'https://vaiotltd.medium.com/vai-token-pre-staking-re-launch-fea723e178a9'}>here</a>
-        </TYPE.white>
       </PositionInfo>
     </PageWrapper>
   )
