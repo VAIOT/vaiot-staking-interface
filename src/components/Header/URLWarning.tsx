da5dadcba4e3633ebbd89f9112c27062ff0dea1f
k.krzyspiak <k.krzyspiak@gmail.com>
2023-01-10 16:01:42 +0100

    URL warning -- address changed

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,15 +30,15 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>staking.vaiot.ai</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>stake.vaiot.ai</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'staking.vaiot.ai' ? (
+  ) : window.location.hostname === 'stake.vaiot.ai' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>staking.vaiot.ai</code> - bookmark it
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>stake.vaiot.ai</code> - bookmark it to
         to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
