d7f996c774687f366eb11d9b933242fbb1622dfd
Yaroslav Dubinskyi <yaroslav.dubinskyi@dac.digital>
2023-03-24 17:39:53 +0100

    Cleanup and add message about wrong network

--- src/pages/VaiStake/index.tsx
+++ src/pages/VaiStake/index.tsx
@@ -27,6 +27,11 @@ import { ButtonPrimary } from '../../components/Button'
 import VaiStackingModal from '../../components/VaiStake/VaiStakeModal'
 import WithdrawTokensModal from 'components/VaiStake/WithdrawTokensModal'
 import WithdrawRewardsModal from 'components/VaiStake/WithdrawRewardsModal'
+import { SupportedChainId } from 'constants/chains'
+import { OutlineCard } from 'components/Card'
+import { switchToNetwork } from 'utils/switchToNetwork'
+
+const SUPPORTED_CHAIN_IDS = [SupportedChainId.POLYGON, SupportedChainId.MUMBAI]
 
 export function parseBigNumber(value: TokenAmount): string {
   return value.greaterThan(BigInt(100000))
@@ -35,8 +40,10 @@ export function parseBigNumber(value: TokenAmount): string {
 }
 
 export default function VaiStake() {
-  const { chainId, account } = useActiveWeb3React()
+  const { chainId, account, library } = useActiveWeb3React()
   const toggleWalletModal = useWalletModalToggle()
+
+  const isChainIdSupported = Boolean(SUPPORTED_CHAIN_IDS.includes(chainId as any))
 
   const token = chainId ? VAI[chainId] : undefined
 
@@ -68,6 +75,12 @@ export default function VaiStake() {
     withdrawRewardModalState.open()
   }, [withdrawRewardModalState])
 
+  const handleSwitchToPolygonNetwork = useCallback(() => {
+    if (library) {
+      switchToNetwork({ library, chainId: SupportedChainId.POLYGON })
+    }
+  }, [library])
+
   return (
     <PageWrapper gap="lg" justify="center">
       <RowBetween style={{ gap: '24px' }}>
@@ -75,117 +88,133 @@ export default function VaiStake() {
         <CurrencyLogo currency={token} size="24px" />
       </RowBetween>
 
-      <DataRow style={{ gap: '24px' }}>
-        <PoolData>
-          <AutoColumn gap="sm">
-            <TYPE.body style={{ margin: 0 }}>Total deposits</TYPE.body>
-            <RowBetween style={{ alignItems: 'baseline' }}>
-              <TYPE.body fontSize={24} fontWeight={500}>
-                {stakingInfo && stakingInfo.totalStakedAmount ? parseBigNumber(stakingInfo.totalStakedAmount) : '0'} /{' '}
-                {stakingInfo && stakingInfo.currentStakingLimit ? parseBigNumber(stakingInfo.currentStakingLimit) : '0'}
-              </TYPE.body>
-              <TYPE.white>VAI</TYPE.white>
-            </RowBetween>
-          </AutoColumn>
-        </PoolData>
-
-        <PoolData>
-          <AutoColumn gap="sm">
-            <TYPE.body style={{ margin: 0 }}>ARR</TYPE.body>
-            <TYPE.body fontSize={24} fontWeight={500}>
-              {stakingInfo ? stakingInfo?.totalRewardRate.toSignificant(6) : '0'}
-              {'%'}
-            </TYPE.body>
-          </AutoColumn>
-        </PoolData>
-      </DataRow>
-
-      <PositionInfo gap="lg" justify="center" dim={null}>
-        <BottomSection gap="lg" justify="center">
-          <StyledDataCard disabled={!hasStackedAmount}>
-            <CardSection>
-              <CardBGImage desaturate />
-              <CardNoise />
-              <AutoColumn gap="md">
-                <RowBetween>
-                  <TYPE.white fontWeight={600}>Your VAI deposits</TYPE.white>
-                </RowBetween>
+      {isChainIdSupported ? (
+        <>
+          <DataRow style={{ gap: '24px' }}>
+            <PoolData>
+              <AutoColumn gap="sm">
+                <TYPE.body style={{ margin: 0 }}>Total deposits</TYPE.body>
                 <RowBetween style={{ alignItems: 'baseline' }}>
-                  <TYPE.white fontSize={36} fontWeight={600}>
-                    {stakingInfo?.stakedAmount?.toSignificant(6) ?? '-'}
-                  </TYPE.white>
+                  <TYPE.body fontSize={24} fontWeight={500}>
+                    {stakingInfo && stakingInfo.totalStakedAmount ? parseBigNumber(stakingInfo.totalStakedAmount) : '0'}{' '}
+                    /{' '}
+                    {stakingInfo && stakingInfo.currentStakingLimit
+                      ? parseBigNumber(stakingInfo.currentStakingLimit)
+                      : '0'}
+                  </TYPE.body>
                   <TYPE.white>VAI</TYPE.white>
                 </RowBetween>
               </AutoColumn>
-            </CardSection>
-          </StyledDataCard>
+            </PoolData>
 
-          <StyledBottomCard dim={!hasStackedAmount}>
-            <CardBGImage desaturate />
-            <CardNoise />
-            <AutoColumn gap="sm">
-              <RowBetween>
-                <div>
-                  <TYPE.black>Your earned VAI</TYPE.black>
-                </div>
-              </RowBetween>
-              <RowBetween style={{ alignItems: 'baseline' }}>
-                <TYPE.largeHeader fontSize={36} fontWeight={600}>
-                  <CountUp
-                    key={countUpAmount}
-                    isCounting
-                    decimalPlaces={4}
-                    start={parseFloat(countUpAmountPrevious)}
-                    end={parseFloat(countUpAmount)}
-                    thousandsSeparator={','}
-                    duration={1}
-                  />
-                </TYPE.largeHeader>
-              </RowBetween>
-            </AutoColumn>
-          </StyledBottomCard>
-        </BottomSection>
+            <PoolData>
+              <AutoColumn gap="sm">
+                <TYPE.body style={{ margin: 0 }}>APR</TYPE.body>
+                <TYPE.body fontSize={24} fontWeight={500}>
+                  {stakingInfo ? stakingInfo?.totalRewardRate.toSignificant(6) : '0'}
+                  {'%'}
+                </TYPE.body>
+              </AutoColumn>
+            </PoolData>
+          </DataRow>
 
-        <DataRow>
+          <PositionInfo gap="lg" justify="center" dim={null}>
+            <BottomSection gap="lg" justify="center">
+              <StyledDataCard disabled={!hasStackedAmount}>
+                <CardSection>
+                  <CardBGImage desaturate />
+                  <CardNoise />
+                  <AutoColumn gap="md">
+                    <RowBetween>
+                      <TYPE.white fontWeight={600}>Your VAI deposits</TYPE.white>
+                    </RowBetween>
+                    <RowBetween style={{ alignItems: 'baseline' }}>
+                      <TYPE.white fontSize={36} fontWeight={600}>
+                        {stakingInfo?.stakedAmount?.toSignificant(6) ?? '-'}
+                      </TYPE.white>
+                      <TYPE.white>VAI</TYPE.white>
+                    </RowBetween>
+                  </AutoColumn>
+                </CardSection>
+              </StyledDataCard>
+
+              <StyledBottomCard dim={!hasStackedAmount}>
+                <CardBGImage desaturate />
+                <CardNoise />
+                <AutoColumn gap="sm">
+                  <RowBetween>
+                    <div>
+                      <TYPE.black>Your earned VAI</TYPE.black>
+                    </div>
+                  </RowBetween>
+                  <RowBetween style={{ alignItems: 'baseline' }}>
+                    <TYPE.largeHeader fontSize={36} fontWeight={600}>
+                      <CountUp
+                        key={countUpAmount}
+                        isCounting
+                        decimalPlaces={4}
+                        start={parseFloat(countUpAmountPrevious)}
+                        end={parseFloat(countUpAmount)}
+                        thousandsSeparator={','}
+                        duration={1}
+                      />
+                    </TYPE.largeHeader>
+                  </RowBetween>
+                </AutoColumn>
+              </StyledBottomCard>
+            </BottomSection>
+
+            <DataRow>
+              {stakingInfo && (
+                <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleDepositClick}>
+                  Deposit
+                </ButtonPrimary>
+              )}
+
+              {hasStackedAmount && (
+                <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleWithdrawTokensClick}>
+                  Withdraw deposit
+                </ButtonPrimary>
+              )}
+
+              {hadEarnedAmount && (
+                <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleWithdrawRewardClick}>
+                  Claim Rewards
+                </ButtonPrimary>
+              )}
+            </DataRow>
+          </PositionInfo>
+
           {stakingInfo && (
-            <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleDepositClick}>
-              Deposit
-            </ButtonPrimary>
-          )}
+            <>
+              <VaiStackingModal
+                isOpen={stakingModalState.isOpen}
+                onDismiss={stakingModalState.close}
+                vaiStakingInfo={stakingInfo}
+              />
 
-          {hasStackedAmount && (
-            <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleWithdrawTokensClick}>
-              Withdraw deposit
-            </ButtonPrimary>
-          )}
+              <WithdrawTokensModal
+                isOpen={withdrawTokensModalState.isOpen}
+                onDismiss={withdrawTokensModalState.close}
+                stakingInfo={stakingInfo}
+              />
 
-          {hadEarnedAmount && (
-            <ButtonPrimary padding="8px" borderRadius="8px" width="160px" onClick={handleWithdrawRewardClick}>
-              Claim Rewards
-            </ButtonPrimary>
+              <WithdrawRewardsModal
+                isOpen={withdrawRewardModalState.isOpen}
+                onDismiss={withdrawRewardModalState.close}
+                stakingInfo={stakingInfo}
+              />
+            </>
           )}
-        </DataRow>
-      </PositionInfo>
-
-      {stakingInfo && (
+        </>
+      ) : (
         <>
-          <VaiStackingModal
-            isOpen={stakingModalState.isOpen}
-            onDismiss={stakingModalState.close}
-            vaiStakingInfo={stakingInfo}
-          />
-
-          <WithdrawTokensModal
-            isOpen={withdrawTokensModalState.isOpen}
-            onDismiss={withdrawTokensModalState.close}
-            stakingInfo={stakingInfo}
-          />
-
-          <WithdrawRewardsModal
-            isOpen={withdrawRewardModalState.isOpen}
-            onDismiss={withdrawRewardModalState.close}
-            stakingInfo={stakingInfo}
-          />
+          <OutlineCard>This network is not supported, please switch to Polygon Mainnet</OutlineCard>
+          <DataRow style={{ justifyContent: 'center' }}>
+            <ButtonPrimary padding="8px" borderRadius="8px" width="200px" onClick={handleSwitchToPolygonNetwork}>
+              Switch to Polygon
+            </ButtonPrimary>
+          </DataRow>
         </>
       )}
     </PageWrapper>
