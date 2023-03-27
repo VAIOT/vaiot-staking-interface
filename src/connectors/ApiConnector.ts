ac77196579bc652b42d83f816215d0bb334229ce
Yaroslav Dubinskyi <yaroslav.dubinskyi@dac.digital>
2023-03-27 12:24:12 +0200

    Fix vai price popup requests

--- src/connectors/ApiConnector.ts
+++ src/connectors/ApiConnector.ts
@@ -1,14 +1,33 @@
-export async function getCirculation(): Promise<{
-  success: boolean
-  data?: { usdPrice: number; circulatingSupply: string; totalSupply: number }
-}> {
+import axios from 'axios'
+
+export async function getCirculation() {
   try {
-    const response = await fetch(`https://api.vaiot.ai/token`)
-    if (!response.ok) {
-      return { success: false }
-    } else {
-      const json = await response.json()
-      return { success: true, data: json }
+    const response = await axios.post<{
+      price: Record<
+        string,
+        {
+          name: string
+          circulating_supply: number
+          quote: {
+            USD: {
+              price: number
+            }
+          }
+        }[]
+      >
+    }>('https://baseapi.vaiot.ai/api/v1/asset/getAssetPrice', {
+      symbol: 'VAI',
+      version: 'v2'
+    })
+
+    const vaiObj = response?.data?.price['VAI'].find(el => el.name === 'VAIOT')
+
+    return {
+      success: true,
+      data: {
+        price: vaiObj?.quote?.USD?.price,
+        circulation: vaiObj?.circulating_supply
+      }
     }
   } catch (error) {
     console.log(error)
